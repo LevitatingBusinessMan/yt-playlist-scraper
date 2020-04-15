dc8f864f433401b26927ce9a8f8e17f43d0e69f1
Rein F <public@reinfernhout.xyz>
2020-04-15 15:30:43 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,180 @@
+const https = require("https"),
+	bestThumbnail = require("./util/bestThumbnail"),
+	formatVideoList = require("./util/formatVideoList"),
+	cutJson = require("./util/cutJson")
+
+require("./util/String.between")
+
+const getPlaylist = ID => new Promise((resolve, reject) => {
+	
+	if (!ID)
+		reject(Error("No ID specified"))
+	
+	const url = new URL("https://www.youtube.com")
+	url.pathname = "/playlist"
+	url.searchParams.append("list", ID)
+
+	// Without the deskopt user agent yt will not send its ytInitialData block of JSON
+	https.get(url,
+		{
+			headers: {
+				"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36"
+			}
+		}, res => {
+		
+		const { statusCode } = res
+		
+		if (statusCode != 200) {
+
+			reject(Error(`Non 200 code received at initial request (received ${statusCode})`))
+			
+			// Consume response to free memory
+			res.resume()
+			return
+
+		}
+
+		let html = ""
+		res.on("data", chunk => html += chunk)
+		res.on("end", () => {
+			
+			require("fs").writeFile("test.html", html, function () {})
+
+			// Array of the videos inside the playlist
+			const ytInitialData = collectData(html)
+
+			//Something went wrong fetching the json and it has been handled
+			if (!ytInitialData)
+				return
+
+			const title = ytInitialData
+				.microformat
+				.microformatDataRenderer
+				.title
+
+			const description = ytInitialData
+			.microformat
+			.microformatDataRenderer
+			.description
+			
+			const thumbnails = {
+				all: ytInitialData
+					.microformat
+					.microformatDataRenderer
+					.thumbnail
+					.thumbnails,
+				best: bestThumbnail(
+						ytInitialData
+						.microformat
+						.microformatDataRenderer
+						.thumbnail
+						.thumbnails
+					)
+			}
+
+			let videos = formatVideoList(ytInitialData)
+			
+			const results = {
+				id: ID,
+				thumbnails,
+				description,
+				title,
+				videos
+			}
+
+			loadMore(ytInitialData)
+
+			function loadMore(ytData) {
+
+				const continuation = ytData
+					.contents
+					.twoColumnBrowseResultsRenderer
+					.tabs[0]
+					.tabRenderer
+					.content
+					.sectionListRenderer
+					.contents[0]
+					.itemSectionRenderer
+					.contents[0]
+					.playlistVideoListRenderer
+					.continuations[0]
+
+				//console.log(continuation)
+				if (!continuation) {
+		
+					resolve(results)
+					return
+
+				}
+					
+
+				const url = new URL("https://www.youtube.com")
+				url.pathname = "/browse_ajax"
+				url.searchParams.append("ctoken", continuation.nextContinuationData.continuation)
+				url.searchParams.append("continuation", continuation.nextContinuationData.continuation)
+				url.searchParams.append("itct", continuation.nextContinuationData.clickTrackingParams)
+
+				//Get the next 100 videos and add them to the list
+				https.get(url, res => {
+					
+					const { statusCode } = res
+		
+					if (statusCode != 200) {
+
+						reject(Error(`Non 200 code received at fetchMore request (received ${statusCode})`))
+						
+						// Consume response to free memory
+						res.resume()
+						return
+
+					}
+
+					let html = ""
+					res.on("data", chunk => html += chunk)
+					res.on("end", () => {
+						
+						// Array of the videos inside the playlist
+						const newInitialData = collectData(html)
+						console.log(typeof newInitialData)
+
+						//Something went wrong fetching the json and it has been handled
+						if (!newInitialData)
+							return
+						
+						results.videos = results.videos.concat(formatVideoList(newInitialData))
+
+						loadMore(newInitialData)
+					
+					})
+				})
+
+			}
+
+		})
+		
+	})
+
+	function collectData(html) {
+
+		const beforeText = "window[\"ytInitialData\"] = "
+		const afterText = ";\n    window[\"ytInitialPlayerResponse\"]"
+	
+		const ytInitialData = html.between(beforeText, afterText)
+	
+		if (!ytInitialData) {
+			reject(Error("Unable to retrieve playlist data"))
+			return
+		}
+
+		try {
+			return JSON.parse(ytInitialData)
+		} catch(err) {
+			reject(Error("Unable to parse JSON (ytInitialData)"))
+			return null
+		}
+	
+	}
+
+})
+
+module.exports = getPlaylist
---
+++ src/util/formatVideoList.js
@@ -0,0 +1,42 @@
+function formatVideoList(ytInitialData) {
+
+	const rawVideoList = ytInitialData
+	.contents
+	.twoColumnBrowseResultsRenderer
+	.tabs[0]
+	.tabRenderer
+	.content
+	.sectionListRenderer
+	.contents[0]
+	.itemSectionRenderer
+	.contents[0]
+	.playlistVideoListRenderer
+	.contents
+
+	const videos = []
+	for (video of rawVideoList) {
+		video = video.playlistVideoRenderer
+
+		const vidElement = {
+			id: video.videoId,
+			title: video.title.simpleText,
+			thumbnails: {
+				best: bestThumbnail(video.thumbnail.thumbnails),
+				all: video.thumbnail.thumbnails
+			},
+			channel: {
+				title: video.shortBylineText.runs[0].text,
+				endpoint: video.shortBylineText.runs[0].navigationEndpoint.browseEndpoint.canonicalBaseUrl,
+				id: video.shortBylineText.runs[0].navigationEndpoint.browseEndpoint.browseId
+			},
+			duration: video.lengthSeconds
+		}
+
+		videos.push(vidElement)
+
+	}
+	return videos
+
+}
+
+module.exports = formatVideoList
